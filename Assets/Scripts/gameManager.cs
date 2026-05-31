439be9a7513bdcf88dd7909da1d1c2cda3a78dc8
Dilsan Basnet <officialdilsanacc@gmail.com>
2026-06-01 00:07:51 +0545

    working on gameover, scoreincrease , playbutton, will do tomorror gn

--- Assets/Scripts/gameManager.cs
+++ Assets/Scripts/gameManager.cs
@@ -1,8 +1,13 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class gameManager : MonoBehaviour
 {
  private int Score;
+ public player player;
+ public Text scoretext;
+ public GameObject PlayButton;
+ public GameObject GameOver;
  public void Gameover()
     {
        Debug.Log("Game Over");
