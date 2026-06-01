bbebe0dce685ecb5082870a7c0c12e903701e339
Dilsan Basnet <officialdilsanacc@gmail.com>
2026-06-01 08:49:56 +0545

    Script added for gameover ui, score points, & PlayButton

--- Assets/Scripts/gameManager.cs
+++ Assets/Scripts/gameManager.cs
@@ -1,20 +1,49 @@
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class gameManager : MonoBehaviour
 {
  private int Score;
  public player player;
- public Text scoretext;
+ public TextMeshProUGUI scoretext;
  public GameObject PlayButton;
  public GameObject GameOver;
- public void Gameover()
+    public void Awake() {
+        Application.targetFrameRate = 60;
+        Pause();
+    }
+    public void Play() {
+        Score = 0;
+        scoretext.text = Score.ToString();
+
+        PlayButton.SetActive(false);
+        GameOver.SetActive(false);
+        Time.timeScale = 1f;
+        player.enabled = true;
+        Pipes[] pipes = FindObjectsByType<Pipes>();
+        for(int i = 0; i < pipes.Length; i++)
+        {
+            Destroy(pipes[i].gameObject);
+        }
+
+    }
+    public void Pause()
     {
-       Debug.Log("Game Over");
+       Time.timeScale = 0f; 
+       player.enabled = false;
+    }
+    public void Gameover() {
+        GameOver.SetActive(true);
+        PlayButton.SetActive(true);
+        Pause();
+
+        Debug.Log("Game Over");
     }
 
 public void ScoreIncrease()
     {
         Score++;
+        scoretext.text = " SCORE : " + Score.ToString();
     }
 }
