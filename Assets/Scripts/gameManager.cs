49edf221f6f553be855db14355d3e57400446577
Dilsan Basnet <officialdilsanacc@gmail.com>
2026-06-01 17:15:08 +0545

    BEST score added , simple scipte for it in gamemanager and highscore text Ugui in scoring

--- Assets/Scripts/gameManager.cs
+++ Assets/Scripts/gameManager.cs
@@ -5,15 +5,19 @@ using TMPro;
 public class gameManager : MonoBehaviour
 {
  private int Score;
+ private int highScore;
  private int diamonds;
  public player player;
  public TextMeshProUGUI scoretext;
  public TextMeshProUGUI diamondText;
+ public TextMeshProUGUI highScoreText;
  public GameObject PlayButton;
  public GameObject GameOver;
  public GameObject ExitButton;
     public void Awake() {
         Application.targetFrameRate = 60;
+        highScore = PlayerPrefs.GetInt("HighScore", 0);
+        updateHighscore();
         Pause();
     }
     public void Play() {
@@ -21,7 +25,7 @@ public class gameManager : MonoBehaviour
         diamonds = 0;
         scoretext.text = "SCORE : " + Score.ToString();
         diamondText.text = "DIAMOND : " + diamonds.ToString();
-
+       updateHighscore();
         PlayButton.SetActive(false);
         GameOver.SetActive(false);
         ExitButton.SetActive(false);
@@ -54,6 +58,18 @@ public void ScoreIncrease()
     {
         Score++;
         scoretext.text = " SCORE : " + Score.ToString();
+
+        if(Score > highScore)
+        {
+            highScore = Score;
+            PlayerPrefs.SetInt("HighScore", highScore);
+
+           updateHighscore();
+        }
+    }
+    private void updateHighscore()
+    {
+        highScoreText.text = "BEST SCORE : " + highScore.ToString();
     }
     public void IncreaseDiamond()
     {
